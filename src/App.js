b4145faa41d73a019e0279c54f961e262291e92b
woong9149 <sewoong0409@naver.com>
2020-06-26 23:01:23 +0900

    component 만들기

--- src/App.js
+++ src/App.js
@@ -1,27 +1,42 @@
-import React from 'react';
+import React,{Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
-  render() {
+class Nav extends Component{
+  render(){
     return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <nav>
+            <ul>
+                <li><a href="1.html">HTML</a></li>  
+                <li><a href="2.html">CSS</a></li>
+                <li><a href="3.html">JavaScript</a></li>
+            </ul>
+        </nav>
+    )
+  }
+}
+
+class Subject extends Component{
+  render(){//class안의 함수는 function을 생략하고 사용 가능
+    //컴포넌트는 최상위태그 하나로 감싸져야한다.
+    return (
+      <header>
+        <h1>WEB</h1>
+        World Wide Web!
       </header>
-    </div>
-  );
+
+    )
+  }
+}
+
+class App extends Component{
+  render(){
+    return (
+      <div className="App">
+        <Subject></Subject>
+        <Nav></Nav>
+      </div>
+    );
 }
 }
 
