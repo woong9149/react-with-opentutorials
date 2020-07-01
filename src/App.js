df0c4885e79781d52e29b57a8fdc01cacbaddc4a
woong9149 <sewoong0409@naver.com>
2020-07-01 23:50:54 +0900

    state 적용

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React,{Component} from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 import TOC from './components/TOC';
 import Content from './components/Content';
@@ -7,11 +7,26 @@ import Subject from './components/Subject';
 
 
 class App extends Component{
+
+  constructor(props){
+    super(props);
+    this.state = {
+      subject:{title:'WEB', sub: 'World Wide Web'},
+      contents:[
+        {id:1, title:'HTML', desc:'HTML is for information...'},
+        {id:2, title:'CSS', desc:'CSS is for design'},
+        {id:3, title:'JavaScript', desc:'JavaScript is for interactive...'}
+      ]
+    }
+
+
+  }
+
   render(){
     return (
       <div className="App">
-        <Subject title="WEB" sub="World Wide Web"></Subject>
-        <TOC title="" desc=""></TOC>
+        <Subject title = {this.state.title} sub = {this.state.sub}></Subject>
+        <TOC data = {this.state.contents}></TOC>
         <Content title="HTML" desc="HTML is HyperText Markup Language."></Content>
       </div>
     );
