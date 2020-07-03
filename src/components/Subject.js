45f4917806fc359b4b371f5e630291bc35ab3ba6
woong9149 <sewoong0409@naver.com>
2020-07-04 00:45:56 +0900

    react - 이벤트

--- src/components/Subject.js
+++ src/components/Subject.js
@@ -5,8 +5,18 @@ class Subject extends Component{
       //컴포넌트는 최상위태그 하나로 감싸져야한다.
       return (
         <header>
-          <h1>{this.props.title}</h1>
-                {this.props.sub}
+          {/* <h1><a href="/">{this.props.title}</a></h1>
+                {this.props.sub} */}
+          <h1><a href="/" onClick={function(e){
+              console.log('e: ',e);
+              e.preventDefault();
+              // this.state.mode = 'Welcome';
+             this.props.onChangePage();
+              // this.setState({
+              //   mode:'Welcome'
+              // })
+            }.bind(this)}>{this.props.title}</a></h1>
+            {this.props.sub}
         </header>
   
       )
