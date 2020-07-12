6d3eafe840321d1901d56dde0605b56d7e9c1959
woong9149 <sewoong0409@naver.com>
2020-07-12 21:27:41 +0900

    update,delete 구현

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ class App extends Component{
     super(props);
     this.max_content_id = 3;
     this.state = {
-      mode:'create',
+      mode:'welcome',
       selected_content_id:2,
       subject:{title:'WEB', sub: 'World Wide Web'},
       welcome:{title:'Welcome', desc:'Hello, React!!'},
@@ -32,45 +32,50 @@ class App extends Component{
       var data = this.state.contents[i];
       if(data.id === this.state.selected_content_id){
         return data;
-        break;
       }
       i = i + 1;
     }
   }
   getContent(){
     console.log('App render');
-    var _title, _desc,_article = null;
+    var _title, _desc,_article= null;
     if(this.state.mode === 'Welcome'){
+      _title = this.state.welcome.title;
+      _desc = this.state.welcome.desc;
       _article = <ReadContent title={_title} desc={_desc}></ReadContent>
     }else if(this.state.mode === 'read'){
       var _content = this.getReadContent();
       _article = <ReadContent title={_content.title} desc={_content.desc}></ReadContent>
     }else if(this.state.mode === 'create'){
       _article = <CreateContent onSubmit = {function(_title,_desc){
-        // add content to this.state.contents
         this.max_content_id = this.max_content_id+1;
-        // this.state.contents.push(
+        // var _contents = this.state.contents.concat(
         //   {id:this.max_content_id, title:_title, desc:_desc}
         // );
-        var _contents = this.state.contents.concat(
-          {id:this.max_content_id, title:_title, desc:_desc}
-        );
+        var _contents = Array.from(this.state.contents);
+        _contents.push({id:this.max_content_id, title:_title,desc:_desc});
         this.setState({
-          contents:_contents
+          contents:_contents,
+          mode:'read',
+          selected_content_id:this.max_content_id
         });
       }.bind(this)}></CreateContent>
     }else if(this.state.mode === 'update'){
-      _article = <UpdateContent data = {_content}onSubmit = {function(_title,_desc){
-        // add content to this.state.contents
-        this.max_content_id = this.max_content_id+1;
-        // this.state.contents.push(
-        //   {id:this.max_content_id, title:_title, desc:_desc}
-        // );
-        var _contents = this.state.contents.concat(
-          {id:this.max_content_id, title:_title, desc:_desc}
-        );
+      _content = this.getReadContent();
+      _article = <UpdateContent data={_content} onSubmit={
+      function(_id, _title, _desc){
+       var _contents = Array.from(this.state.contents);
+          var i = 0;
+          while(i < _contents.length){
+            if(_contents[i].id === _id) {
+              _contents[i] = {id:_id, title:_title, desc:_desc};
+              break;
+            }
+            i = i + 1;
+          }
         this.setState({
-          contents:_contents
+          contents:_contents,
+          mode:"read"
         });
       }.bind(this)}></UpdateContent>
     }
@@ -94,9 +99,29 @@ class App extends Component{
           })
         }.bind(this)} data = {this.state.contents} ></TOC>
         <Control onChangeMode={function(_mode){
-          this.setState({
-            mode:_mode
-          });
+          if(_mode === 'delete'){
+            if(window.confirm('delete?')){
+              var _contents = Array.from(this.state.contents);
+              var i = 0;
+              while(i < _contents.length){
+                if(_contents[i].id === this.state.selected_content_id){
+                  _contents.splice(i,1);
+                  break;
+                }
+                i = i + 1;
+              }
+              this.setState({
+                mode:'welcome',
+                contents:_contents
+              });
+              alert('delete completed !')
+            }
+          }else{
+            this.setState({
+              mode:_mode
+            });
+          }
+          
         }.bind(this)}></Control>
         {this.getContent()}
       </div>
--- src/components/UpdateContent.js
+++ src/components/UpdateContent.js
@@ -1,29 +1,58 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 
-class UpdateContent extends Component {
+class UpdateContent extends Component{
+    constructor(props){
+      super(props);
+      this.state = {
+        title:this.props.data.title,
+        desc:this.props.data.desc,
+        id:this.props.data.id
+      }
+      this.inputFormHandler = this.inputFormHandler.bind(this);
+    }
+    inputFormHandler(e){
+      this.setState({[e.target.name]:e.target.value});
+    }
     render(){
-      console.log('data: ',this.props.data);
-      console.log('UpdateContent render !');
+      console.log(this.props.data);
+      console.log('UpdateContent render');
       return (
         <article>
-              <h2>Update</h2>
-              <form action="/create_process" method="post" onSubmit={function(e){
+            <h2>Update</h2>
+            <form action="/create_process" method="post"
+              onSubmit={function(e){
                 e.preventDefault();
-                this.props.onSubmit(e.target.title.value,e.target.title.value);
-              }.bind(this)}>
-                <p>
-                  <input type="text" name="title" placeholder="title"></input>
+                this.props.onSubmit(
+                 this.state.id,
+                 this.state.title,
+                 this.state.desc
+                );
+              }.bind(this)}
+            >
+              <input type="hidden" name="id" value={this.state.id}></input>
+              <p>
+                <input 
+                  type="text" 
+                  name="title" 
+                  placeholder="title"
+                  value={this.state.title}
+                  onChange={this.inputFormHandler}
+                ></input>
                 </p>
-                <p>
-                  <textarea name="desc" placeholder="description"></textarea>
-                </p>
-                <p>
-                  <input type="submit"></input>
-                </p>
-              </form>
-          </article>
-      )
+              <p>
+                <textarea 
+                  onChange={this.inputFormHandler} 
+                  name="desc" 
+                  placeholder="description" 
+                  value={this.state.desc}></textarea>
+              </p>
+              <p>
+                <input type="submit"></input>
+              </p>
+            </form>
+        </article>
+      );
     }
   }
 
-  export default UpdateContent;
+export default UpdateContent;
